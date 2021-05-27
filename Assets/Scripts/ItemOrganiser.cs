0e210d3ca8768e61410c51521fca26d007ee9fd9
MiniMECH <minimechpop@pop-os.localdomain>
2021-05-27 22:00:04 +0100

    Added needed things and started scripts

---
+++ Assets/Scripts/ItemOrganiser.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOrganiser : MonoBehaviour
+{
+
+    public GameObject objectForContainer;
+    public GameObject container;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void refresh_list()
+    {
+        Debug.Log("This should refresh the list");
+    }
+}
